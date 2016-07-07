87fc74e144f7c8f66bd8f06d0152fc5cf22477ce
schulzsebastian <schulz.siwy@gmail.com>
2016-07-07 18:02:36 +0200

    clear repo

--- app/ranking.service.ts
+++
@@ -1,8 +0,0 @@
-export class RankingService {
-	getPlayers() : string[] {
-		return [{name:'Sebastian',points:9,games:3},
-		{name:'Łukasz',points:8,games:2},
-		{name:'Wojciech',points:7,games:1},
-		{name:'Aleksandra',points:6,games:0}]
-	}
-}
