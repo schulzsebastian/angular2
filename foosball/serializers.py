d3cfb1a4a1e49606a42e03dadaf0e57166423651
schulzsebastian <schulz.siwy@gmail.com>
2016-07-11 21:37:46 +0200

    clear

--- foosball/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from foosball.models import Player
-
-
-class PlayerSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Player
-        fields = ('id', 'name', 'points', 'games')
--- foosball/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views, api
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^getplayers/', api.get_players, name='get_players'),
-    url(r'^addplayer/', api.add_player, name='add_player'),
-    url(r'^addpoints/', api.add_points, name='add_points')
-]
