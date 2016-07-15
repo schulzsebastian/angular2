333e3bb652dccd7c185c300281ac6662a4839864
schulzsebastian <schulz.siwy@gmail.com>
2016-07-15 17:44:18 +0200

    api fix

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^plan/(?P<pid>[0-9]+)$', views.plan_by_id, name='plan_by_id'),
-    url(r'^user/(?P<uid>[0-9]+)$', views.user_by_id, name='user_by_id')
+    url(r'^plans$', views.get_plans, name='get_plans'),
+    url(r'^plans/(?P<uid>[0-9]+)$', views.plans_by_user, name='plans_by_user'),
+    url(r'^user/(?P<uid>[0-9]+)$', views.user_by_id, name='user_by_id'),
+    url(r'^users$', views.get_users, name='get_users')
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from .models import Plan, User
@@ -8,11 +8,31 @@ def index(request):
     return JsonResponse({'status':'ok'})
 
 
+def get_plans(request):
+    plans = get_list_or_404(Plan)
+    response = {'data':[model_to_dict(plan) for plan in plans]}
+    return JsonResponse(response, json_dumps_params={'indent':4})
+
+
+def plans_by_user(request, uid):
+    plans = get_list_or_404(Plan, user_id=uid)
+    response = {'data':[model_to_dict(plan) for plan in plans]}
+    return JsonResponse(response, json_dumps_params={'indent':4})
+
+
 def plan_by_id(request, pid):
     plan = model_to_dict(get_object_or_404(Plan, id=pid))
-    return JsonResponse(plan)
+    response = {'data':plan}
+    return JsonResponse(response, json_dumps_params={'indent':4})
+
+
+def get_users(request):
+    users = get_list_or_404(User)
+    response = {'data':[model_to_dict(user) for user in users]}
+    return JsonResponse(response, json_dumps_params={'indent':4})
 
 
 def user_by_id(request, uid):
     user = model_to_dict(get_object_or_404(User, id=uid))
-    return JsonResponse(user)
+    response = {'data':user}
+    return JsonResponse(response, json_dumps_params={'indent':4})
